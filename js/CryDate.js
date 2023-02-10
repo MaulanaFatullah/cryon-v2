8814c5cca4d8a2731076f4e5d419e5a69df62fbc
Maulana Fatullah <fistalanid@gmail.com>
2023-02-11 06:16:21 +0700

    Optimizing performance

--- js/CryDate.js
+++ js/CryDate.js
@@ -1,12 +1,1 @@
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-let month = today.getMonth();
-let yyyy = today.getFullYear();
-
-today = monthNames[month] + ' ' + dd;
-document.getElementById('dateNow').innerText = "It's " + today;
-document.getElementById('yearNow').innerText = "©" + yyyy;
-document.getElementById('yearNowTop').innerText = "©" + yyyy;
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; let today = new Date, dd = String(today.getDate()).padStart(2, "0"), mm = String(today.getMonth() + 1).padStart(2, "0"), month = today.getMonth(), yyyy = today.getFullYear(); today = monthNames[month] + " " + dd, document.getElementById("dateNow").innerText = "It's " + today, document.getElementById("yearNow").innerText = "\xa9" + yyyy, document.getElementById("yearNowTop").innerText = "\xa9" + yyyy;
