5ab81e89cf89c8957e729dea29b9c898d85b5ff0
Maulana Fatullah <fistalanid@gmail.com>
2023-02-10 22:03:26 +0700

    Add some buttons and create favicon

--- js/CryNav.js
+++ js/CryNav.js
@@ -1,7 +1,1 @@
-const navBtn = document.getElementById('nav-mobile-btn');
-const nameTag = document.getElementById('name-tag');
-
-navBtn.addEventListener('click', () => {
-    navBtn.classList.toggle('active');
-    nameTag.classList.toggle('active');
-});
+const navBtn = document.getElementById('nav-mobile-btn'); const nameTag = document.getElementById('name-tag'); navBtn.addEventListener('click', () => { navBtn.classList.toggle('active'); nameTag.classList.toggle('active') })
